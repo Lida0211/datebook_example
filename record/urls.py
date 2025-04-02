272fd48deb28a9dae5931fcb5636f6b9d294da5b
Лидия Лапанович <kazachenok199@mail.ru>
2025-04-02 16:32:59 +0300

    link_create

--- record/urls.py
+++ record/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from record.views import index
+from record.views import index, index_id
 
 
 urlpatterns = [
-    path('', index),
+    path('', index, name = 'records'),
+    path('<int:record_id>',index_id, name = 'record'),
 ]
--- record/views.py
+++ record/views.py
@@ -11,3 +11,12 @@ def index(request):
     #menu = Menu.objects.order_by()
     context = {"records":records}
     return HttpResponse(template.render(context, request))
+
+
+def index_id(request, record_id):
+    template = loader.get_template("record/index_id.html")
+    record = Record.objects.get(pk = record_id)
+    #days = Day.objects.order_by("time")
+    #menu = Menu.objects.order_by()
+    context = {"record":record}
+    return HttpResponse(template.render(context, request))
